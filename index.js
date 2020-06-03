744fe1a894f23aeb87a852324e640a47a2af2952
henriquetroiano <henriquetroiano@hotmail.com>
2020-06-03 11:28:00 -0300

    adição de reload automático do browser ao index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var reload = require('reload')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -8,4 +9,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 //   res.sendFile(path.join(__dirname + '/public/index.html'));
 // });
 
-app.listen(5500);
+
+app.listen(5500, () => {
+    console.log('Ouvindo a http://127.0.0.1:5500')
+});
+
+reload(app)
